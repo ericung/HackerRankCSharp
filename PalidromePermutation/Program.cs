3ddde458018d0d63059d9859867dfde5da9e2a94
pietore <ungericw@tutanota.com>
2018-08-12 10:25:20 -0500

    unrefined palidromepermutation checker

---
+++ PalidromePermutation/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PalidromePermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ataoata";
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+
+            foreach(var c in s)
+            {
+                if (letters.ContainsKey(c))
+                {
+                    letters[c]++;
+                }
+                else
+                {
+                    letters[c] = 1;
+                }
+            }
+
+            bool isPalindrome = true;
+            bool containsOdd = false;
+
+            if (s.Length % 2 == 1)
+            {
+                containsOdd = true;
+            }
+
+            foreach(var element in letters)
+            {
+                if (element.Value % 2 == 1)
+                {
+                    if (containsOdd == true)
+                    {
+                        containsOdd = false;
+                    }
+                    else
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+
+            }
+        }
+    }
+}
